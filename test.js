fd0e96803f8113a2bd58f0db9c20218f3ca5734e
Amit Gupta <amit.gupta@daffodilsw.com>
2019-04-13 13:22:24 +0530

    fc

---
+++ test.js
@@ -0,0 +1,17 @@
+// const a = 'there was 1 goat. 1 goat was ill. Other was a Lion. Lion was king. King ate goat';
+
+// const b = a.split('.');
+
+// const d = b.filter(c=>{
+//    return !/goat/.test(c);
+// });
+
+// console.log(d.join('.'));
+
+const QUOTESMAP = new Map();
+QUOTESMAP.set("1","First, solve the problem. Then, write the code.");
+QUOTESMAP.set("2","Java is to JavaScript what car is to Carpet.");
+QUOTESMAP.set("3","In order to be irreplaceable, one must always be different");
+QUOTESMAP.set("4","Simplicity is the soul of efficiency");
+
+console.log(QUOTESMAP);
